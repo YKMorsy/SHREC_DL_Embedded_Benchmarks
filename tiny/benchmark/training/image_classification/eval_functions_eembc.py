cc639795bc35d26ad157c3673b8a32f9cf4e85da
Yasser Morsy <54084895+YKMorsy@users.noreply.github.com>
2023-03-16 14:56:24 -0400

    Update eval_functions_eembc.py

--- tiny/benchmark/training/image_classification/eval_functions_eembc.py
+++ tiny/benchmark/training/image_classification/eval_functions_eembc.py
@@ -19,6 +19,9 @@ import numpy as np
 # labels are the correct answers
 def calculate_accuracy(y_pred, labels):
     y_pred_label = np.argmax(y_pred, axis=1)
+    print(y_pred_label)
+    print("--------")
+    print(labels)
     correct = np.sum(labels == y_pred_label)
     accuracy = 100 * correct / len(y_pred)
     print(f"Overall accuracy = {accuracy:2.1f}")
